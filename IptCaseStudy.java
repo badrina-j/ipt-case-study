72d14091d4399d199ace9f65bb78abb0ecc22155
PepitoDexter <imlolilover@gmail.com>
2021-03-20 14:59:17 +0800

    Created README and Java file

---
+++ IptCaseStudy.java
@@ -0,0 +1,14 @@
+public class IptCaseStudy {
+    public char CharAt(String a, int b){
+        return a.charAt(b);
+    }
+    
+    public int IndexOf(String a,char b){
+        return a.indexOf(b);
+    }
+    
+    public boolean Contains(String a, String b){
+        return a.contains(b);
+    }
+
+}
